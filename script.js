1cfee4b86803ff48f986cb7958855d481387c16b
myronmalyk <154856395+myronmalyk@users.noreply.github.com>
2024-11-10 09:06:05 -0800

    First Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('reserva-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('¡Tu reserva ha sido realizada exitosamente!');
+  });
+  
